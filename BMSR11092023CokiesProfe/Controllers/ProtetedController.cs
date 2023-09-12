cc01991b840a7b27d8b14753516ea77254d9366b
Bryan Misael Sanchez Ramirez <misalbryan@gmail.com>
2023-09-11 19:38:36 -0600

    Creacion de copia de ejercicio

---
+++ BMSR11092023CokiesProfe/Controllers/ProtetedController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BMSR11092023CokiesProfe.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    //Aplica la autorizacion a este controlador, lo que significa solo
+    //los usuarios autenticados podran acceder a sus acciones
+    [Authorize]
+    public class ProtetedController : ControllerBase
+    {
+        //Crea una lista de objetos 'data' para almacenar informacion
+        static List<object> data = new List<object>();
+
+        [HttpGet]
+        public IEnumerable<object> Get()
+        {
+            //Devuelve los datos almacenados en la lista 'data' en respuesta a una solicitud GET
+            return data;
+        }
+        [HttpPost]
+        public IActionResult Post(string name, string lastname)
+        {
+            //Agrega un nuevo objeto anonimo con 'name' y 'lastName'
+            //a la lista 'data' en respuesta a una solicitud POST 
+            data.Add(new { name, lastname });
+            //Devuelve una respuesta HTTP exitosa
+            return Ok();
+        }
+    }
+}
